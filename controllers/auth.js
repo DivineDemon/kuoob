81625cc13d53229632db98e9432c241bb09dd67d
DivineDemon <supame123@gmail.com>
2023-08-27 11:50:14 +0500

    Authentication Fixes! 🚀✨

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,40 +1,89 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { PrismaClient } = require("@prisma/client");
+
 const fs = require("fs").promises;
+const prisma = new PrismaClient();
+const SECRET = process.env.JWT_SECRET;
+
+const register = async (req, res) => {
+  try {
+    const { username, email, password, user_status, display_name } = req.body;
+    const encryptedPassword = bcrypt.hashSync(password, 10, (err, hash) => {
+      if (!err) {
+        return hash;
+      } else {
+        res.status(400).json({
+          status: false,
+          message: "Password Encryption Failed!",
+          err,
+        });
+      }
+    });
+
+    const response = await prisma.wp_users.create({
+      data: {
+        user_login: username,
+        user_pass: encryptedPassword,
+        user_nicename: username,
+        user_email: email,
+        user_registered: new Date().toISOString(),
+        user_status,
+        display_name,
+      },
+    });
+
+    res.status(200).json({
+      status: true,
+      message: "Registered User Successfully!",
+      user: response.ID,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: false,
+      message: "Please Try Again!",
+      error: error.message,
+    });
+  }
+};
 
 const login = async (req, res) => {
   try {
-    let jsonData = {};
-    const { username, password } = req.body;
+    const { email, password } = req.body;
+    const user = await prisma.wp_users.findUnique({
+      where: {
+        user_email: email,
+      },
+    });
 
-    const response = await fetch(
-      `https://kuoob.com/wp-json/jwt-auth/v1/token?username=${username}&password=${password}`,
-      { method: "POST" }
-    );
-
-    const data = await response.text();
-    let check = data.includes("code") ? true : false;
-
-    if (check) {
-      jsonData = JSON.parse(
-        "{" + data.split("{")[1] + "{" + data.split("{")[2]
+    if (bcrypt.compareSync(password, user.user_pass)) {
+      // Generate JWT Token
+      const userToken = jwt.sign(
+        {
+          id: user.ID,
+          email: user.email,
+        },
+        SECRET,
+        {
+          expiresIn: "30d",
+        }
       );
-      return res.status(jsonData.data.status).json({
-        success: false,
-        message: jsonData.message,
-        error: jsonData.code,
+      // Send User Data
+      res.status(200).json({
+        success: true,
+        message: "User Logged In!",
+        user,
+        token: userToken,
       });
     } else {
-      jsonData = "{" + data.split("{")[1];
-      await fs.writeFile("./token.txt", jsonData);
+      res.status(401).json({
+        status: false,
+        message: "Username or Password Incorrect!",
+      });
     }
-
-    return res.status(200).json({
-      success: true,
-      message: "Successfully Logged In!",
-      user: JSON.parse(jsonData),
-    });
   } catch (error) {
-    return res.status(500).json({
-      success: false,
+    res.status(500).json({
+      status: false,
       message: "Please Try Again!",
       error: error.message,
     });
@@ -42,5 +91,6 @@ const login = async (req, res) => {
 };
 
 module.exports = {
+  register,
   login,
 };
